69158309207f33ce834ba9f7399fbd9c339a25f1
Ubuntu <ubuntu@ip-172-31-21-68.eu-north-1.compute.internal>
2024-08-17 08:37:52 +0000

    first commit to check errors

---
+++ server/src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.get('/', (req, res) => {
+  res.send('Hello from the server!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
---
+++ server/src/routes.js
@@ -0,0 +1,8 @@
+// Example route file
+const express = require('express');
+const router = express.Router();
+
+// Define your routes here
+
+module.exports = router;
+
